d1aa655eb22f1d81a6c4f19ea629b25d78f9eda5
Khalidprithy <khalidprithy@gmail.com>
2022-07-07 21:49:40 +0600

    MongoDB connected

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const { MongoClient, ServerApiVersion, MongoRuntimeError } = require('mongodb');
 require('dotenv').config()
 const port = process.env.PORT || 5000;
 
 app.use(cors());
+app.use(express.json());
+
+
+
+
+const uri = "mongodb+srv://cruduser:naXJR4nVj*Wc6Ra@cluster0.ta851.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const productCollection = client.db("simpleCrud").collection("products");
+
+    }
+    finally {
+        // await client.close();
+    }
+}
+
+run().catch(console.dir)
+
 
 app.get('/', (req, res) => {
     res.send("Running CRUD server")
@@ -13,3 +35,7 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log("Server is running")
 })
+
+
+// cruduser
+// naXJR4nVj*Wc6Ra
